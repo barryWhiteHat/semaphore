ed7a569471ba8e6476f7c8f9302dd9329893ae52
Harry <HarryR@users.noreply.github.com>
2018-08-29 13:28:25 +0100

    Added tests for LongsightF322 in Solidity

--- test/TestLongsightF.sol
+++ test/TestLongsightF.sol
@@ -6,7 +6,7 @@ import "../contracts/LongsightF.sol";
 
 contract TestLongsightF
 {
-	function testKnown () public
+	function testKnownF12 () public
 	{
 		uint256[12] memory round_constants;
 		LongsightF.ConstantsF12p5(round_constants);
@@ -16,6 +16,20 @@ contract TestLongsightF
 		uint256 expected = 14698330907891059605542875061480191518388361330761392167383706051673460062628;
 
 		uint256 result = LongsightF.LongsightF12p5(x_L, x_R, round_constants);
+
+		Assert.equal(result, expected, "Unexpected result");
+	}
+
+	function testKnownF322 () public
+	{
+		uint256[322] memory round_constants;
+		LongsightF.ConstantsF322p5(round_constants);
+
+		uint256 x_L = 3703141493535563179657531719960160174296085208671919316200479060314459804651;
+		uint256 x_R = 134551314051432487569247388144051420116740427803855572138106146683954151557;
+		uint256 expected = 1955118202659622298192442035507501123132991419752400995882287708761535290053;
+
+		uint256 result = LongsightF.LongsightF322p5(x_L, x_R, round_constants);
 
 		Assert.equal(result, expected, "Unexpected result");
 	}
